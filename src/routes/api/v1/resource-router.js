7a4afbdbd06be9cbca51726275003d58c6e2b1fd
Miranda Holmlund <mirreanda@hotmail.com>
2023-04-25 10:44:10 +0200

    Added some messages to routes

--- src/routes/api/v1/resource-router.js
+++ src/routes/api/v1/resource-router.js
@@ -5,6 +5,10 @@ export const router = express.Router()
 
 const resolvedResourceController = (req) => req.app.get('container').resolve('ResourceController')
 
+router.get('/', () => {
+  res.json({ message : "/source/:index/:source for getting data from a source query. /term-aggregation/:index/:terms/:sum to get a certain term aggregated data."})
+})
+
 router.get('/resource/source/:index/:source', async (req, res) => {
     await resolvedResourceController(req).getSourceResource(req, res)
 })
--- src/routes/api/v1/router.js
+++ src/routes/api/v1/router.js
@@ -3,4 +3,8 @@ import { router as resourceRouter } from './resource-router.js'
 
 export const router = express.Router()
 
+router.get('/', () => {
+  res.json({ message : "Welcome to this simple API for fetching data from my Elastic Cloud. /resources for more."})
+})
+
 router.use('/', resourceRouter)
